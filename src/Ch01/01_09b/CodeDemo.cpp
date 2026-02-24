103429a0284e8b988e9d00fa5bb44cbe3079bbaa
jtchoy <58255300+jtchoy@users.noreply.github.com>
2026-02-24 10:06:28 +0000

    added

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,15 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name: " << std::flush; // to ensure it prints before user to input
+
+    std::cin >> name;
+
+    std::cout << "Nice to meet you, " << name << "!" <<std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
